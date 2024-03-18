UtsavBalar1231/platform_device_xiaomi_pipa

pipa: parts: Introduce Dolby Atmos
Based on existing dirac implementation and observing stock
sound effects app and daxservice.

Thanks to jhenrique09 for the hack from old dirac parts
"Pause/play music stream to get effects applied".

TODO: bring back misound (same as stock)

Co-authored-by: Henrique Silva <jhenrique09.mcz@hotmail.com>
Change-Id: Ia137963efee2f54a74b391dc5ea5016fc60a8fab
Signed-off-by: Mesquita <mesquita@aospa.co>

parts/src/co/aospa/settings/BootCompletedReceiver.java
@@ -1,18 +1,9 @@
 /*
  * Copyright (C) 2015 The CyanogenMod Project
  *               2017-2020 The LineageOS Project
+ * Copyright (C) 2023 Paranoid Android
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 package co.aospa.settings;
@@ -24,6 +15,7 @@ import android.util.Log;
 
 import co.aospa.settings.thermal.ThermalUtils;
 import co.aospa.settings.refreshrate.RefreshUtils;
+import co.aospa.settings.dolby.DolbyUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -35,5 +27,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         ThermalUtils.startService(context);
         RefreshUtils.startService(context);
+        DolbyUtils.getInstance(context).onBootCompleted();
     }
 }

kuroringo90/android_device_xiaomi_sm8250-common

parts/src/org/lineageos/settings/BootCompletedReceiver.java
@@ -1,18 +1,9 @@
 /*
  * Copyright (C) 2015 The CyanogenMod Project
  *               2017-2020 The LineageOS Project
+ * Copyright (C) 2023 Paranoid Android
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * SPDX-License-Identifier: Apache-2.0
  */
 
 package org.lineageos.settings;
@@ -24,6 +15,7 @@ import android.util.Log;
 
 import org.lineageos.settings.popupcamera.PopupCameraUtils;
 import org.lineageos.settings.thermal.ThermalUtils;
+import org.lineageos.settings.dolby.DolbyUtils;
 
 public class BootCompletedReceiver extends BroadcastReceiver {
 
@@ -35,5 +27,6 @@ public class BootCompletedReceiver extends BroadcastReceiver {
         if (DEBUG) Log.d(TAG, "Received boot completed intent");
         PopupCameraUtils.checkPopupCameraService(context);
         ThermalUtils.startService(context);
+        DolbyUtils.getInstance(context).onBootCompleted();
     }
 }